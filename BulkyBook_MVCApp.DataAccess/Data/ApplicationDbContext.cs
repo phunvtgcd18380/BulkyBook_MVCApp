d9afd4d5ae4d4eb17e8ba13879ce4fe99a32352f
phunvtgcd18380 <66460872+phunvtgcd18380@users.noreply.github.com>
2021-07-04 23:20:10 +0700

    createDatabaseAndCategoryTable

--- BulkyBook_MVCApp.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook_MVCApp.DataAccess/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using BulkyBook_MVCApp.Models;
 
 namespace BulkyBook_MVCApp.DataAccess.Data
 {
@@ -12,5 +13,6 @@ namespace BulkyBook_MVCApp.DataAccess.Data
             : base(options)
         {
         }
+        public DbSet<Category> Categories { get; set; }
     }
 }
